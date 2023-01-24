8ec6b1f542a11a7dece3b61fd6c45f3457fcd373
Vinicius Maier <108704347+viniciusmaier@users.noreply.github.com>
2023-01-24 02:17:37 -0300

    Add files via upload

---
+++ js/animatedString.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () =>{
+    new TypeIt(".animated", {
+        speed: 400,
+        strings: 'Front-End',
+        loop: true
+        
+    }).go()
+})
