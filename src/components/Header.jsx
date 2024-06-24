b9fffa107a41fb9cab3b0f35a4200e6ee1ea420f
viniciusmaier <viniciusfiel1000@gmail.com>
2024-06-24 13:22:35 -0300

    feat : ajustando o header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,28 @@
 import { Icons } from "../../public/icons/Icons";
-export default function HeaderPage(){
-    return(
-      <header className= "w-screen h-12 bg-black bg-opacity-85">
-        <div className="flex justify-center items-center h-full"> 
-          <div className="flex items-center justify-between w-full max-w-screen-xl px-4">
-            <div className="text-blue-500 font-semibold flex flex-row gap-2">
-              <i className="text-blue-500"><Icons.computer/></i>
-              <h3>V-M</h3>
-            </div>
+import { Roboto } from "next/font/google"
 
-            <div className="text-blue-500">
-              <a href="#" className="mr-4 p-3.5 hover:bg-lime-900 transition duration-700">projects</a> 
-              <a href="#" className="mr-4 p-2">contacts</a> 
-              <a href="#" className="mr-4 p-2">about</a>
-            </div>
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: "400",
+});
+
+export default function HeaderPage() {
+  return (
+    <header className="w-screen h-12 bg-[#050D0C] bg-opacity-90 fixed">
+      <div className="flex justify-center items-center h-full outline outline-[#e02d8196] outline-offset-4">
+        <div className="flex items-center justify-between w-full max-w-screen-xl px-4">
+          <div className={`${roboto.className} text-[#F22786] flex flex-row gap-2`}>
+            <i className="text-[#F22786]"><Icons.computer /></i>
+            <span className="font-extrabold">V-M</span>
+          </div>
+
+          <div className="text-[#F22786] font-extrabold">
+            <a href="#" className="mr-4 p-3.5 hover:bg-lime-900 transition duration-700">AboutMe</a>
+            <a href="#" >Projects</a>
+            <a href="#" className="mr-4 p-2">Contacts</a>
           </div>
         </div>
-      </header>
-    );
+      </div>
+    </header>
+  );
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,12 @@
 import type { AppProps } from "next/app";
 //import '@/src/styles/reset.css'
 import '@/src/styles/globals.css'
+import Header from "../components/Header";
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+    </>
+  );
 }
