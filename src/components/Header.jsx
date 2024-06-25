35b669d7e97ae00151928228f5541e7cd19023f6
viniciusmaier <viniciusfiel1000@gmail.com>
2024-06-25 13:28:56 -0300

    feat : Ajustando legenda

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,25 @@
 import { Icons } from "../../public/icons/Icons";
 import { Roboto } from "next/font/google"
-
-const roboto = Roboto({
+import { Alegreya } from "next/font/google"
+const fontAlegreya = Alegreya({
   subsets: ["latin"],
   weight: "400",
 });
 
 export default function HeaderPage() {
   return (
-    <header className="w-screen h-12 bg-[#050D0C] bg-opacity-90 fixed">
-      <div className="flex justify-center items-center h-full outline outline-[#e02d8196] outline-offset-4">
+    <header className={`w-screen h-12 bg-[#050D0C] bg-opacity-90 fixed`}>
+      <div className={`flex justify-center items-center h-full outline outline-[#7e244e96] outline-offset-4 ${fontAlegreya.className} text-xl`}>
         <div className="flex items-center justify-between w-full max-w-screen-xl px-4">
-          <div className={`${roboto.className} text-[#F22786] flex flex-row gap-2`}>
+          <div className={`text-[#F22786] flex flex-row gap-2`}>
             <i className="text-[#F22786]"><Icons.computer /></i>
             <span className="font-extrabold">V-M</span>
           </div>
 
-          <div className="text-[#F22786] font-extrabold">
-            <a href="#" className="mr-4 p-3.5 hover:bg-lime-900 transition duration-700">AboutMe</a>
-            <a href="#" >Projects</a>
-            <a href="#" className="mr-4 p-2">Contacts</a>
+          <div className="text-[#F22786] font-bold text">
+            <a href="#" className="mr-5 p-3 hover:text-[#e6e6e696] transition duration-700">AboutMe</a>
+            <a href="#" className="mr-5 p-3 hover:text-[#e6e6e696] transition duration-700">Projects</a>
+            <a href="#" className="mr-5 p-3 hover:text-[#e6e6e696] transition duration-700">Contacts</a>
           </div>
         </div>
       </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,10 @@
 import Header from "@/src/components/Header"
-import TypeIt from "typeit-react";
+import AnimationString from "@/src/components/utils/animationStrings";
 import { Roboto } from "next/font/google"
-import { Alegreya } from "next/font/google"
+
 const fontRoboto = Roboto({
   subsets: ['latin'],
   weight: "900"
-});
-
-const fontAlegreya = Alegreya({
-  subsets : ["latin"],
-  weight : "900"
 });
 
 export default function Home() {
@@ -17,38 +12,25 @@ export default function Home() {
     <main>
       <div className="w-screen h-[55rem] bg-cover bg-center bg-no-repeat" style={{ backgroundImage: 'url(/imgs/backGround.gif)' }}>
         <div className="w-96 h-full flex justify-end flex-col text-center">
-          <TypeIt
-            className={`${fontAlegreya.className} text-[#00ffffc9] font-extrabold text-[30px]`}
-            getBeforeInit={
-              (animation) => {
-                animation
-                  .type("Programmer", {delay : 500})
-                  .pause(400)
-                  .empty()
-                  .type("Web Developer", {delay : 500})
-                  .pause(400, {delay : 500})
-                  .move(-9, {delay : 500})
-                  .delete(5, {delay : 500})
-                  .move(12, {delay : 500})
-                  .type(`<span style="color: #F22786;"> Front-End </span>`, {delay :500})
-                  .delete(10, {delay : 500})
-                  .pause(500)
-                  .type(`<span style="color: #ff7c02c9;"><em><strong> Back-End</strong></em><span>`, {delay : 500})
-
-                  return animation;
-              } 
-            }
-
-            ></TypeIt>
+          <AnimationString />
           <img src="/imgs/perfil.png" className="w-full animate-personaMove my-10" alt="teste" />
         </div>
         <div className="bg-gradient-to-b from-midnight-blue to-black w-full h-[30rem] flex justify-center text-white">
           <div className="flex flex-col w-[35rem] h-[15rem] text-center justify-evenly">
-            <div className={`${fontRoboto.className} font-bold -rotate-2 animate-rgbLetter text-letterRgb text-center`}>About Me</div>
+            <div className={`${fontRoboto.className} font-bold -rotate-2 text-letterRgb text-center`}>
+              <span className="transition animate-rgbLetter delay-150">A</span>
+              <span className="transition animate-rgbLetter delay-200">b</span>
+              <span className="transition animate-rgbLetter delay-250">o</span>
+              <span className="transition animate-rgbLetter delay-[300]">u</span>
+              <span className="transition animate-rgbLetter delay-[350]">t</span>
+              <span> </span>
+              <span className="transition animate-rgbLetter delay-[400]">M</span>
+              <span className="transition animate-rgbLetter delay-[450]">e</span>
+            </div>
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis, voluptate quam accusantium porro quis, quia officiis sunt, doloremque vitae neque voluptatem adipisci nesciunt officia eligendi corporis. Dolore sunt iusto nam?</p>
           </div>
         </div>
       </div>
-    </main>
+    </main >
   );
 }
